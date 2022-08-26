7823b2e189b302ebe268a0bd1af2c6887e044437
oeg1n18 <oeg1n18@soton.ac.uk>
2022-08-26 09:16:56 +0100

    Added Two Policy Methods to DQNAgent. Next need to implement method to train Qnetwork from trajectory

--- agents/DQNAgent.py
+++ agents/DQNAgent.py
@@ -3,22 +3,38 @@ import tensorflow as tf
 from tensorflow import keras
 from replay_buffers.UniformReplayMemory import *
 
-def DQNAgent:
-    def __init__(self, qnet, replay_mem, learning_rate=0.01, df=0.99, epsilon_decay=0.005):
-        self.replay_mem = replay_mem
+class DQNAgent:
+    def __init__(self, observation_space, action_space, qnet, learning_rate=0.01, df=0.99, epsilon_decay=0.005):
+        self.epsilon = None
         self.learning_rate = learning_rate
         self.df = df
         self.epsilon_decay = epsilon_decay
         self.episode = 0
         self.qnet = qnet
+        self.observation_space = observation_space
+        self.action_space = action_space
 
     def collect_policy(self, trajectory):
-        if trajectory.done == True:
+        if trajectory.done:
             self.episode += 1
-            self.epsilon ==
+            self.epsilon = np.exp(-self.epsilon_decay*self.episode)
+        if np.rnadon.rand() < self.epsilon:
+            try:
+                action = np.random.randint(self.action_space.n)
+            except:
+                print("Action space must have attribute action_space.n")
+        else:
+            qvalues = self.Qnet(np.expand_dims(trajectory.state, axis=0))
+            action = np.argmax(qvalues)
+        return int(qvalues)
 
-        self.epsilon =
+    def policy(self, trajectory):
+        qvalues = self.Qnet(np.expand_dims(trajectory.state, axis=0))[0]
+        action = np.argmax(qvalues)
+        return int(qvalues)
 
-    def policy():
 
-    def train(self, experiences):
+
+
+
+
